e42c7f929ae0bd2d5b91d4569079d76508cb3798
Keti <kbach19@freeuni.edu.ge>
2023-06-04 19:09:52 +0400

    Navbar init

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import NavigationBar from './NavigationBar';
+
+function App() {
+  return (
+    <NavigationBar></NavigationBar>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fff'
+        },
+        material: {
+            main: 'rgb(236,102,82)'
+        }
+    }
+})
+
+root.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+);
