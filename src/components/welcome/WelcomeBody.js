6dcdf836efb72b6bc7e4b7ef035880fba91a04c2
Ketevan Bachalashvili <kbach19@freeuni.edu.ge>
2023-07-28 03:51:45 +0400

    make welcome body responsive and delete fullpage js

--- src/components/welcome/WelcomeBody.js
+++ src/components/welcome/WelcomeBody.js
@@ -1,8 +1,11 @@
-import { Button, Container, Grid } from "@mui/material";
+import { Button, Container, Grid, useMediaQuery } from "@mui/material";
 import { useEffect, useRef } from "react";
+import "@fontsource/roboto";
 import Typed from "typed.js";
 
 function WelcomeBody() {
+    const isLargeScreen = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
     const typedRef = useRef(null);
 
     useEffect(() => {
@@ -15,7 +18,6 @@ function WelcomeBody() {
             parse: true
         }
 
-        
         // Create a new instance of Typed.js
         typedRef.current = new Typed('.typed-header', options);
 
@@ -26,12 +28,12 @@ function WelcomeBody() {
 
 
     return (
-        <Container maxWidth="xl" style={{marginTop: '50px'}}>
-            <Grid container spacing={2}>
-                <Grid item xs={6} style={{marginTop: '200px'}}>
-                    <h1 style={{marginTop: "0px", textAlign: "center"}} className="header-title">Learn to <span style={{color: "rgb(236, 102, 82)"}} className="typed-header"></span></h1>
-                    <h3 style={{marginTop: "15px",  textAlign: "center", color: "#d5ebf7", letterSpacing: "1px"}}>Learn and sharpen your skill with <span style={{fontWeight: "bold"}}>Study Map</span></h3>
-                    <div style={{display: "flex", alignItems: "baseline", justifyContent: "center", gap: "20px"}}>
+        <Container maxWidth="xl">
+            <Grid container spacing={2} alignItems="center" style={{ marginTop: "70px" }}>
+                <Grid item xs={isLargeScreen ? 6 : 12} order={isLargeScreen ? 1 : 2}>
+                    <h1 style={{ marginTop: "0px", textAlign: "center", fontFamily: "cubano" }} className="header-title">Learn to <span style={{ color: "rgb(236, 102, 82)" }} className="typed-header"></span></h1>
+                    <h3 style={{ marginTop: "15px", textAlign: "center", color: "#d5ebf7", letterSpacing: "1px", fontFamily: "roboto"}}>Learn and sharpen your skill with <span style={{ fontWeight: "bold" }}>Study Map</span></h3>
+                    <div style={{ display: "flex", alignItems: "baseline", justifyContent: "center", gap: "20px" }}>
                         <Button sx={{
                             width: "130px",
                             fontFamily: "cubano",
@@ -46,8 +48,8 @@ function WelcomeBody() {
                         }} variant="contained" color="light">Sign Up</Button>
                     </div>
                 </Grid>
-                <Grid item xs={6} style={{textAlign: "center"}}>
-                    <img src="/woman.png" style={{width: "500px", marginTop: "25px"}} alt="Welcome" />
+                <Grid item xs={isLargeScreen ? 6 : 12} style={{ display: "flex", justifyContent: "center" }} order={isLargeScreen ? 2 : 1}>
+                    <img src="/woman.png" style={{ width: "350px", margin: '0 auto', maxWidth: "100%" }} alt="Welcome" />
                 </Grid>
             </Grid>
         </Container>
--- src/components/welcome/WelcomeContent.js
+++ src/components/welcome/WelcomeContent.js
@@ -1,24 +1,13 @@
-import ReactFullpage from "@fullpage/react-fullpage";
 import NavigationBar from "../navbar/NavigationBar";
 import WelcomeBody from "./WelcomeBody";
 
 
 function WelcomeContent() {
     return (
-        <ReactFullpage scrollingSpeed={1000}
-            render={({state, fullPageApi}) => {
-                return (
-                    <ReactFullpage.Wrapper>
-                        <div className="section" style={{minHeight: "100vh", display: "block"}}>
-                            <NavigationBar />
-                            <WelcomeBody />
-                        </div>
-                        <div className="section" style={{minHeight: "100vh", display: "block"}}>
-                            <h1>Test</h1>
-                        </div>
-                    </ReactFullpage.Wrapper>
-                );
-        }} />
+        <div>
+            <NavigationBar />
+            <WelcomeBody />
+        </div>
     )
 }
 
